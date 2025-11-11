1b1de20e2218ccc01ba6489d5a9c3c3bc0eb13ff
zerotohero200222 <aebharath17@gmail.com>
2025-11-11 18:24:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,23 +42,3 @@ resource "google_api_gateway_gateway" "gateway" {
     environment = var.environment
   }
 }
-
-########################################################
-# Outputs
-########################################################
-
-output "api_gateway_name" {
-  description = "The name of the API Gateway instance"
-  value       = google_api_gateway_gateway.gateway.name
-}
-
-output "api_gateway_region" {
-  description = "The region of the deployed API Gateway"
-  value       = google_api_gateway_gateway.gateway.location
-}
-
-output "api_gateway_url" {
-  description = "The public URL of the API Gateway"
-  value       = google_api_gateway_gateway.gateway.default_hostname
-}
-
