8b4d0ca67ef48e659eb7c04f1f1587b629a21e16
zerotohero200222 <aebharath17@gmail.com>
2025-11-11 18:02:12 +0530

    Refactor comments in main.tf for API resources
    Updated comments to improve clarity and consistency.

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
-# Create the API
+# API Gateway API
 resource "google_api_gateway_api" "api" {
+  provider     = google-beta
   api_id       = "${var.api_id}-${var.environment}"
   display_name = "${var.api_display_name}-${var.environment}"
 }
 
-# Create API Config
+# API Config
 resource "google_api_gateway_api_config" "api_config" {
+  provider      = google-beta
   api           = google_api_gateway_api.api.name
   api_config_id = "config-${var.environment}"
   openapi_documents {
@@ -14,13 +16,15 @@ resource "google_api_gateway_api_config" "api_config" {
       contents = file("${path.module}/openapi.yaml")
     }
   }
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
-# Create API Gateway
+# Gateway
 resource "google_api_gateway_gateway" "gateway" {
+  provider    = google-beta
   name        = "${var.api_id}-gateway-${var.environment}"
   api_config  = google_api_gateway_api_config.api_config.name
   location    = var.region
