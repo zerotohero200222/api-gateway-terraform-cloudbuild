0f37b9d47e99ef1e1eadef6858bea28976a46564
zerotohero200222 <aebharath17@gmail.com>
2025-11-11 18:25:26 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
 output "api_gateway_name" {
-  description = "The name of the API Gateway"
+  description = "The name of the API Gateway instance"
   value       = google_api_gateway_gateway.gateway.name
 }
 
+output "api_gateway_region" {
+  description = "The region of the deployed API Gateway"
+  value       = google_api_gateway_gateway.gateway.location
+}
+
 output "api_gateway_url" {
-  description = "The endpoint URL of the API Gateway"
+  description = "The public URL of the API Gateway"
   value       = google_api_gateway_gateway.gateway.default_hostname
 }
