6e47163ab07e21d5f634f917691cf107175d91f8
zerotohero200222 <aebharath17@gmail.com>
2025-11-11 18:09:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,12 @@
-# API Gateway API
-resource "google_api_gateway_api" "api" {
-  provider     = google-beta
-  api_id       = "${var.api_id}-${var.environment}"
-  display_name = "${var.api_display_name}-${var.environment}"
-}
-
-# API Config
 resource "google_api_gateway_api_config" "api_config" {
   provider      = google-beta
   api           = google_api_gateway_api.api.name
   api_config_id = "config-${var.environment}"
+
   openapi_documents {
     document {
       path     = "openapi.yaml"
-      contents = file("${path.module}/openapi.yaml")
+      contents = filebase64("${path.module}/openapi.yaml")
     }
   }
 
@@ -22,10 +15,3 @@ resource "google_api_gateway_api_config" "api_config" {
   }
 }
 
-# Gateway
-resource "google_api_gateway_gateway" "gateway" {
-  provider    = google-beta
-  name        = "${var.api_id}-gateway-${var.environment}"
-  api_config  = google_api_gateway_api_config.api_config.name
-  location    = var.region
-}
