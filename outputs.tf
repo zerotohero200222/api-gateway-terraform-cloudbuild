0e4d0cbb0b52121556ca19ea5a0c83aa7da48f06
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 08:08:59 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,24 @@
-output "api_gateway_name" {
-  description = "The name of the API Gateway instance"
-  value       = google_api_gateway_gateway.gateway.name
+output "lb_ip" {
+  description = "Global Load Balancer static IP address"
+  value       = google_compute_global_address.lb_ip.address
 }
 
-output "api_gateway_region" {
-  description = "The region of the deployed API Gateway"
-  value       = google_api_gateway_gateway.gateway.location
+output "ssl_certificate_domains" {
+  description = "Domains covered by the Google-managed SSL certificate"
+  value       = google_compute_managed_ssl_certificate.lb_cert.managed[0].domains
 }
 
-output "api_gateway_url" {
-  description = "The public URL of the API Gateway"
-  value       = google_api_gateway_gateway.gateway.default_hostname
+output "url_map_self_link" {
+  description = "Self link of the URL map used by the load balancer"
+  value       = google_compute_url_map.lb_urlmap.self_link
+}
+
+output "https_proxy_self_link" {
+  description = "Self link of the target HTTPS proxy"
+  value       = google_compute_target_https_proxy.lb_proxy.self_link
+}
+
+output "forwarding_rule_self_link" {
+  description = "Self link of the global forwarding rule"
+  value       = google_compute_global_forwarding_rule.lb_forwarding_rule.self_link
 }
